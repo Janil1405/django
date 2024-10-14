eae60586d4c3b4efe40f1d876d5b0ee1e4ec3ecc
Janilson Alves da Silva <99848376+Janil1405@users.noreply.github.com>
2024-10-14 20:47:32 -0300

    oi

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dono, Balcao, Fruta
 
+admin.site.register(Dono)
+admin.site.register(Balcao)
+admin.site.register(Fruta)
 # Register your models here.
