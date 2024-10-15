612b9c65e1d065a1bf9e1459129861734e00ff2f
Janilson Alves da Silva <99848376+Janil1405@users.noreply.github.com>
2024-10-15 20:34:27 -0300

    oi
    oi

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Aluno
+from .serializers import AlunoSerializer
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
